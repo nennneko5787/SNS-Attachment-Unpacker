ea45c7e89b4f8245a77f4eefeb5cfb31ccc5da04
Nennneko5787 <nennneko5787@gmail.com>
2024-02-29 19:14:07 +0900

    ephemeral=Trueを忘れていたので修正をかけました

--- main.py
+++ main.py
@@ -138,7 +138,7 @@ async def on_dropdown(interaction: discord.Interaction):
 								fileList.append(fi)
 
 			if matched:
-				await interaction.followup.send(files=fileList)
+				await interaction.followup.send(files=fileList, ephemeral=True)
 			else:
 				await interaction.followup.send("SNSのリンクまたは画像が見つかりませんでした。", ephemeral=True)
 		except:
